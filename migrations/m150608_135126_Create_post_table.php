d011e098d323de916fc3922c29838676ea392d69
jephby <jephby@gmail.com>
2015-06-08 15:00:02 +0100

    first commit

---
+++ migrations/m150608_135126_Create_post_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150608_135126_Create_post_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150608_135126_Create_post_table cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
