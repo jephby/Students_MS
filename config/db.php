8434120ea0252dacde927f564b6d7e31d0cc86ae
jephby <jephby@gmail.com>
2015-06-08 13:41:42 +0100

    third commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=smsDB',
+    'dsn' => 'mysql:host=localhost;dbname=smsdb',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
